ef0538ffb1c65fc09609d9a5dddad8d8155998cc
Dajan Medina <dmedina2150dev@gmail.com>
2022-05-07 21:26:19 -0400

    REspaldo curso

---
+++ redux-basic/README.md
@@ -0,0 +1,5 @@
+# Usar ts-node desde el proyecto [npm install ts-node --save-dev]
+**./node_modules/.bin/ts-node file-name.ts**
+
+# Usar ts-node instalado globalmente [npm install -g ts-node]
+**ts-node file-name.ts**
---
+++ redux-basic/app-1.ts
@@ -0,0 +1,49 @@
+// Acciones
+
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+
+function reducer( state = 10, action: Action ) {
+
+    switch (action.type) {
+        case 'INCREMENTAR':
+            return state += 1;
+        case 'DECREMENTAR':
+                return state -= 1;
+        case 'MULTIPLICAR':
+            return state * action.payload;
+        case 'DIVIDIR':
+            return state / action.payload;
+        default:
+            return state;
+    }
+}
+
+
+// Usar el reducer
+
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+};
+console.log( reducer(10, incrementadorAction) );
+
+const decremtadorAction: Action = {
+    type: 'DECREMENTAR'
+};
+
+console.log( reducer(10, decremtadorAction) );
+
+const multiplicarAction: Action = {
+    type: 'MULTIPLICAR',
+    payload: 2
+};
+console.log( reducer(10, multiplicarAction) );
+
+const dividirAction: Action = {
+    type: 'DIVIDIR',
+    payload: 3
+};
+console.log( reducer(10, dividirAction) );
