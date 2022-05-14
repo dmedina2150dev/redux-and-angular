3f9c68b3a138da9e36dbdc7a0dac717ba1cee4a3
Dajan Medina <dmedina2150dev@gmail.com>
2022-05-14 18:53:22 -0400

    proyecto arriba en firebase, configuraciones

---
+++ ingreso-egreso-app/server.js
@@ -0,0 +1,12 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/ingreso-egreso-app/'));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/ingreso-egreso-app/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
